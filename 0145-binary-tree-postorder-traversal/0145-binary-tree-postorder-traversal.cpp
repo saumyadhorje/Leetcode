e0e5f1d2eb51d4be94a23d0d37c0b2bb102ad711
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-05-31 17:36:23 +0530

    0145-BINARY-TREE-POSTORDER-TRAVERSAL Stats: Time: 0 ms (100%), Space: 11.1 MB (43.43%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -4,37 +4,22 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        // Vector to store the result of postorder traversal
-        vector<int> result;
-        // Stack to facilitate the traversal of nodes
-        stack<TreeNode*> stack;
-        TreeNode* currentNode = root;
-
-        // Traverse the tree while there are nodes to process
-        while (root != nullptr || !stack.empty()) {
-            if (root != nullptr) {
-                // Add current node's value to result list before going to its
-                // children
-                result.push_back(root->val);
-                // Push current node onto the stack
-               stack.push(root);
-                // Move to the right child
-                root = root->right;
-            } else {
-                // Pop the node from the stack and move to its left child
-                root = stack.top();
-                stack.pop();
-                root= root->left;
-            }
-        }
-        // Reverse the result list to get the correct postorder sequence
-        reverse(result.begin(), result.end());
-        return result;
+        vector<int>res;
+        postorder(root, res);
+        return res;
+    }
+    void postorder(TreeNode* root, vector<int>&res){
+        if(root==nullptr)return;
+        postorder(root->left, res);
+        postorder(root->right , res);
+        res.push_back(root->val);
     }
 };
