619221749692eb48402ea41753be998ee9758322
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-07-11 18:32:39 +0530

    4356-SUM-OF-INTEGERS-WITH-MAXIMUM-DIGIT-RANGE Stats: Time: 0 ms (100%), Space: 31 MB (74.2%) - LeetHub

--- 4356-sum-of-integers-with-maximum-digit-range/4356-sum-of-integers-with-maximum-digit-range.cpp
+++ 4356-sum-of-integers-with-maximum-digit-range/4356-sum-of-integers-with-maximum-digit-range.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
+    int digit_range(int num){
+        int max_dig = INT_MIN, min_dig = INT_MAX;
+        int digit ;
+        while(num){
+            digit = num%10;
+            max_dig = max(max_dig,digit); // stores the maximum digit of the num
+            min_dig = min(min_dig,digit); // stores the minimum digit of the num
+            num /= 10;
+        }
+        return max_dig - min_dig; // returns the difference of the maximum and minimum digits of the num
+    }
     int maxDigitRange(vector<int>& nums) {
-        int ans = 0;
-        int maxRange = -1;
-
-        for (int num : nums) {
-            int temp = num;
-            int largest = 0;
-            int smallest = 9;
-
-            while (temp > 0) {
-                int digit = temp % 10;
-                largest = max(largest, digit);
-                smallest = min(smallest, digit);
-                temp /= 10;
+        // first calculate the digit range of all the elements
+        // then compare the digit range if it is maximum and same then add the elements 
+        // if it is greater update the sum and the max_digit_range
+        // if less then no need to do anything just continue
+        int max_dig_range = INT_MIN,sum; 
+        for(int i=0;i<nums.size();i++){
+            int dig_range = digit_range(nums[i]); // calculate the digit range of the element
+            if(dig_range > max_dig_range){ // if the current element digitrange is greater than the previously stored max digit range then update the things
+                max_dig_range = dig_range ; // updating the max_digit_range
+                sum = nums[i]; // updating the sum
             }
-
-            int range = largest - smallest;
-
-            if (range > maxRange) {
-                maxRange = range;
-                ans = num;
-            } else if (range == maxRange) {
-                ans += num;
+            else if(dig_range == max_dig_range){ 
+                sum += nums[i]; // as we need to return all the elements sum , which have the same max_digit_range
             }
         }
-
-        return ans;
+        return sum;
     }
 };
