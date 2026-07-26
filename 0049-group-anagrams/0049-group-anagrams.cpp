671491440b232f8a01516b8ac7e1f29ce1b9d832
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-07-26 22:10:01 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 8 ms (96.48%), Space: 26 MB (49.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string , vector<string>>mp;
-        vector<vector<string>>ans;
-        for(auto x:strs){
-            string word=x;
-            sort(word.begin(), word.end());
-            mp[word].push_back(x);
-            
+
+        // Map:
+        // Key   -> Sorted word (Example: "aet")
+        // Value -> All words having the same sorted form
+        // Example: {"eat", "tea", "ate"}
+        unordered_map<string, vector<string>> mp;
+
+        // Traverse every string in the input
+        for (int i = 0; i < strs.size(); i++) {
+
+            // Make a copy of the current word
+            string key = strs[i];
+
+            // Sort the copied word
+            // Example:
+            // "eat" -> "aet"
+            // "tea" -> "aet"
+            sort(key.begin(), key.end());
+
+            // Store the original word in the map
+            // If the key already exists, it is added to the same group
+            mp[key].push_back(strs[i]);
         }
-         for(auto x:mp){
-             ans.push_back(x.second);
-         }
-           
+
+        // Vector to store the final answer
+        vector<vector<string>> ans;
+
+        // Traverse all groups stored in the map
+        for (auto x : mp) {
+
+            // Add each group to the answer
+            ans.push_back(x.second);
+        }
+
+        // Return all grouped anagrams
         return ans;
     }
 };
