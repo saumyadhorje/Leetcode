6cb56ca6006b91ce1e61904a770035ddc23d0d30
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-08-07 21:55:46 +0530

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 0 ms (100%), Space: 97.4 MB (25.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // BF
-        // int maxprofit=0;
-        // for(int i=0;i<prices.size();i++){
-        //     for(int j=i+1;j<prices.size();j++){
-        //          int profit=prices[j]-prices[i];
-        //        maxprofit=max(maxprofit,profit);
-        //     }
-        //   }
-        // return maxprofit;
-         int buyPrice = prices[0];
-        int profit = 0;
+        int l = 0, r = 1;
+        int maxP = 0;
 
-        for (int i = 1; i < prices.size(); i++) {
-            if ( prices[i]<buyPrice) {
-                buyPrice = prices[i];
+        while (r < prices.size()) {
+            if (prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxP = max(maxP, profit);
+            } else {
+                l = r;
             }
-
-            profit = max(profit, prices[i] - buyPrice);
+            r++;
         }
 
-        return profit; 
+        return maxP;
     }
 };
