70b6df1796854695348f6d326f5a8ee646a83102
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-08-08 18:11:58 +0530

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        int n = arr.size(); // taking the size of the array
-        
-        int ans = 0; // ans variable to store our count
-        
-        for(int i = 0; i < n; i++) // traverse from the array
-        {
-            int sum = arr[i]; // provide sum with arr[i]
-            
-            if(sum == k) // if element itself equal to k, then also increment count
-                ans++;
-            
-            for(int j = i + 1; j < n; j++) // now moving forward,
-            {
-                sum += arr[j]; // add elements with sum
-                
-                if(sum == k) // if at any point they become equal to k
-                    ans++; // increment answer
+    int subarraySum(vector<int>& nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==k) count++;
+                else{
+                    i++;
+                    j++;
+                }
             }
-            
         }
-        
-        return ans; // and at last, return answer
+        return count;
     }
 };
-
