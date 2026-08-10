e2fcb07d9fada3f69a3871222b452b138a88e60b
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-08-10 09:51:30 +0530

    0907-KOKO-EATING-BANANAS Stats: Time: 4 ms (87.01%), Space: 22.9 MB (54.9%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1, r = *max_element(piles.begin(), piles.end());
-        int ans = r;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            long hours = 0;
-            for (int p : piles)
-                hours += (p + mid - 1) / mid;
+        int low = 1;
+        int high = 0;
 
-            if (hours <= h) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        // Find the biggest pile
+        for (int i = 0; i < piles.size(); i++) {
+            high = max(high, piles[i]);
+        }
+
+        // Binary search for the minimum speed
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            long totalHours = 0;
+
+            // Calculate total hours at speed mid
+            for (int i = 0; i < piles.size(); i++) {
+                totalHours += (piles[i] + mid - 1) / mid;
+            }
+
+            if (totalHours <= h) {
+                // mid works, so try a smaller speed
+                high = mid - 1;
+            }
+            else {
+                // mid is too slow, so try a bigger speed
+                low = mid + 1;
             }
         }
 
-        return ans;
+        return low;
     }
 };
