ca09372569b10e79bbecca723e98381546285ff4
Saumya Dhorje <saumya.dhorje24@vit.edu>
2026-08-25 22:18:55 +0530

    4080-SMALLEST-MISSING-MULTIPLE-OF-K Stats: Time: 7 ms (9.55%), Space: 25.2 MB (24.36%) - LeetHub

--- 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        vector<int>res;
+        unordered_set<int> present(nums.begin(), nums.end());
 
-        for(int i=0;i<nums.size();i++) {
-            if(nums[i]%k==0)
-                res.push_back(nums[i]);
+        int multiple = k;
+
+        while (present.count(multiple)) {
+            multiple += k;
         }
 
-        int x=k;
-
-        while(true) {
-            bool found=false;
-
-            for(int i=0;i<res.size();i++) {
-                if(res[i]==x)
-                    found=true;
-            }
-
-            if(!found)
-                return x;
-
-            x+=k;
-        }
+        return multiple;
     }
 };
